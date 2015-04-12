875e68f8e17c8979cb4c296c95929aea71df7689
dennislixin <dennislixin@gmail.com>
2015-04-13 01:49:38 +0900

    Implement a generic type of max heap

--- MaxHeapTest.java
+++ MaxHeapTest.java
@@ -2,15 +2,125 @@ import java.util.*;
 
 public class MaxHeapTest{
   public static void main(String[] args){
-    MaxHeap maxHeap = new MaxHeap(Arrays.asList());
+    MaxHeap maxHeap = new MaxHeap<Integer>(Arrays.asList(new Integer[]{1,2,3,4}));
     System.out.println(maxHeap);
   }
 }
 
-class MaxHeap{
+class MaxHeap <E extends Comparable<? super E>>{
+  List<E> list;
+
+  public MaxHeap(List<E> list){
+    if(list == null)
+      throw new IllegalArgumentException("list cannot be null");
+    this.list = list;
+    buildHeap();
+  }
+
+  public void buildHeap(){
+    for(int i=lastParent(); i >= 0; i--)
+      maxHeapify(i);
+  }
+
+  private void maxHeapify(int parent){
+    if(isParent(parent)){
+      //assume left child is bigger for now
+      int biggerChild = 2 * parent + 1;
+      int rightChild = biggerChild+1;
+      if(rightChild < list.size() && compare(rightChild, biggerChild) > 0)
+        biggerChild = rightChild;
+
+      if(compare(biggerChild, parent) > 0){
+        swap(biggerChild, parent);
+        maxHeapify(biggerChild);
+      }
+    }
+  }
+
+  private int compare(int fir, int sec){
+    return list.get(fir).compareTo(list.get(sec));
+  }
+
+  public boolean add(E item){
+    if(item == null)
+      return false;
+
+    list.add(item);
+    int child = last();
+    int parent = getParent(child);
+
+    while(isParent(parent)){
+      if(compare(parent, child) < 0){
+        swap(parent, child);
+        child = parent;
+        parent = getParent(parent);
+      }
+      else
+        break;
+    }
+    return true;
+  }
+
+  private void swap(int left, int right){
+    Collections.swap(list, left, right);
+  }
+
+  public E remove(){
+    if(isEmpty())
+      return null;
+
+    swap(0, last());
+    E max = list.remove(last());
+    maxHeapify(0);
+    return max;
+  }
+
+  public boolean isEmpty(){
+    return list.size() == 0;
+  }
+
+  private int last(){
+    return list.size() - 1;
+  }
+
+  private int getParent(int index){
+    if(index == 0)
+      return -1;
+    else
+      return (index-1)/2;
+  }
+
+  private boolean isParent(int index){
+    return index >= 0 && index <= lastParent();
+  }
+
+  private int lastParent(){
+    return list.size()/2 - 1;
+  }
+
+
+  public String toString(){
+    StringBuilder builder = new StringBuilder();
+    int lineCounter = 0;
+    int lineMax = 1;
+    for(E item : list){
+      builder.append(item);
+      builder.append(" ");
+      lineCounter++;
+      if(lineCounter >= lineMax){
+        builder.append("\n");
+        lineCounter = 0;
+        lineMax++;
+      }
+    }
+    return builder.toString();
+  }
+}
+
+class MaxHeap1{
   List<Integer> list;
 
-  public MaxHeap(List<Integer> list){
+  public MaxHeap1(List<Integer> list){
     for(Integer num : list)
       this.list.add(num);
     buildHeap();
