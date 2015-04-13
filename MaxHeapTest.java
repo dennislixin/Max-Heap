1cdb6298c652f208f323c059036ab12b34175883
dennislixin <dennislixin@gmail.com>
2015-04-14 01:11:25 +0900

    Add a few test cases for max heap

--- MaxHeapTest.java
+++ MaxHeapTest.java
@@ -2,18 +2,40 @@ import java.util.*;
 
 public class MaxHeapTest{
   public static void main(String[] args){
-    MaxHeap maxHeap = new MaxHeap<Integer>(Arrays.asList(new Integer[]{1,2,3,4}));
-    System.out.println(maxHeap);
+    test(new Integer[]{1,2,3,4});
+    test(new Integer[]{});
+  }
+
+  public static void test(Integer[] array){
+    MaxHeap<Integer> maxHeap = new MaxHeap<>(array);
+    Random random = new Random();
+    for(int i=0; i<100; i++){
+      Integer num = random.nextInt();
+      maxHeap.add(num);
+    }
+
+    int last = Integer.MAX_VALUE;
+    while(maxHeap.size() > 0){
+      int cur = maxHeap.remove();
+      if(last < cur){
+        System.out.println("wrong");
+        break;
+      }
+      last = cur;
+    }
   }
 }
 
 class MaxHeap <E extends Comparable<? super E>>{
   List<E> list;
 
+  public MaxHeap(E[] array){
+    this(Arrays.asList(array));
+  }
   public MaxHeap(List<E> list){
     if(list == null)
       throw new IllegalArgumentException("list cannot be null");
-    this.list = list;
+    this.list = new ArrayList<E>(list);
     buildHeap();
   }
 
@@ -27,7 +49,7 @@ class MaxHeap <E extends Comparable<? super E>>{
       //assume left child is bigger for now
       int biggerChild = 2 * parent + 1;
       int rightChild = biggerChild+1;
-      if(rightChild < list.size() && compare(rightChild, biggerChild) > 0)
+      if(rightChild < size() && compare(rightChild, biggerChild) > 0)
         biggerChild = rightChild;
 
       if(compare(biggerChild, parent) > 0){
@@ -65,6 +87,10 @@ class MaxHeap <E extends Comparable<? super E>>{
     Collections.swap(list, left, right);
   }
 
+  public int size(){
+    return list.size();
+  }
+
   public E remove(){
     if(isEmpty())
       return null;
@@ -76,11 +102,11 @@ class MaxHeap <E extends Comparable<? super E>>{
   }
 
   public boolean isEmpty(){
-    return list.size() == 0;
+    return size() == 0;
   }
 
   private int last(){
-    return list.size() - 1;
+    return size() - 1;
   }
 
   private int getParent(int index){
@@ -95,7 +121,7 @@ class MaxHeap <E extends Comparable<? super E>>{
   }
 
   private int lastParent(){
-    return list.size()/2 - 1;
+    return size()/2 - 1;
   }
 
 
@@ -105,101 +131,6 @@ class MaxHeap <E extends Comparable<? super E>>{
     int lineMax = 1;
     for(E item : list){
       builder.append(item);
-      builder.append(" ");
-      lineCounter++;
-      if(lineCounter >= lineMax){
-        builder.append("\n");
-        lineCounter = 0;
-        lineMax++;
-      }
-    }
-    return builder.toString();
-  }
-}
-
-class MaxHeap1{
-  List<Integer> list;
-
-  public MaxHeap1(List<Integer> list){
-    for(Integer num : list)
-      this.list.add(num);
-    buildHeap();
-  }
-
-  private void buildHeap(){
-    for(int i = lastParent(); i >= 0; i--)
-      maxHeapify(i);
-  }
-
-  private void maxHeapify(int index){
-    if(isParent(index)){
-      int biggerChild = index * 2 + 1;
-      int rightChild = biggerChild + 1;
-      if(rightChild < list.size() && list.get(biggerChild) < list.get(rightChild))
-        biggerChild = rightChild;
-
-      if(list.get(index) < list.get(biggerChild)){
-        Collections.swap(list, index, biggerChild);
-        maxHeapify(biggerChild);
-      }
-    }
-  }
-
-  private void add(Integer num){
-    list.add(num);
-    int parent = lastParent();
-
-    while(parent != -1){
-      maxHeapify(parent);
-      parent = getParent(parent);
-    }
-  }
-
-  private Integer removeMax(){
-    if(!isEmpty()){
-      Collections.swap(list, 0, last());
-      int max = list.remove(last());
-      maxHeapify(0);
-      return max;
-    }
-    return null;
-  }
-
-  private boolean isParent(int index){
-    return index >= 0 &&  index <= lastParent() ;
-  }
-
-  private int getParent(int childIndex){
-    if(childIndex == 0)
-      return -1;
-    else
-      return (childIndex - 1) / 2;
-  }
-
-  private boolean isLeaf(int index){
-    return index > lastParent() && index < list.size();
-  }
-
-  //return -1 if list is empty
-  private int lastParent(){
-    return list.size()/2 - 1;
-  }
-
-  //return -1 if list is empty
-  private int last(){
-    return list.size() - 1;
-  }
-
-  private boolean isEmpty(){
-    return list.size() == 0;
-  }
-
-  public String toString(){
-    StringBuilder builder = new StringBuilder();
-    int lineCounter = 0;
-    int lineMax = 1;
-    for(Integer num : list){
-      builder.append(num);
       builder.append(" ");
       lineCounter++;
       if(lineCounter >= lineMax){
